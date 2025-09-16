09a604e7c922f06f664b3cce8b8252655cbf2cb4
Nguyen Tran <trannguyenhn2000@gmail.com>
2025-09-16 22:55:17 +0700

    challenge: how to use passport strategies

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
 
 const app = express();
+app.use(cors())
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
@@ -13,9 +19,69 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' });
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+passport.initialize()
+passport.session()
+passport.authenticate('local');
+
+
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (password !== user.password) return done(null, false);
+      return done(null, user);
+    });
+  }));
+  
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login',
+      showLogin: true
+    });
+  });
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id)
+  })
+  
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  })
+  
+  app.post('/login', passport.authenticate('local', { failureRedirect: '/'}) ,
+  (req, res) => {
+    res.redirect('/profile')
+  })
+
+  app.get('/profile', (req, res) => {
+    res.render('profile')
+  })
+  
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
